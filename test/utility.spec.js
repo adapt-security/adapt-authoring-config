f3d3feaff0c50fceeb433fe945252a59ae6f9697
Thomas Taylor <thomas.taylor@kineo.com>
2019-08-26 17:39:52 +0100

    Make sure un-implemented tests fail

--- test/utility.spec.js
+++ test/utility.spec.js
@@ -1,26 +1,24 @@
+const assert = require('assert');
+
 describe('Config utility', function() {
   describe('#get()', function() {
     it('should be able to retrieve a value', function() {
-      // assert.equal([1, 2, 3].indexOf(4), -1);
-      // assert.equal(false);
+      assert(false);
     });
   });
   describe('#set()', function() {
     it('should be able to set a value', function() {
-      // assert.equal([1, 2, 3].indexOf(4), -1);
-      // assert.equal(false);
+      assert(false);
     });
   });
   describe('#get()', function() {
     it('should be able to verify a value exists', function() {
-      // assert.equal([1, 2, 3].indexOf(4), -1);
-      // assert.equal(false);
+      assert(false);
     });
   });
   describe('#getPublicConfig()', function() {
     it('should be able to retrieve values marked as public', function() {
-      // assert.equal([1, 2, 3].indexOf(4), -1);
-      // assert.equal(false);
+      assert(false);
     });
   });
 });
