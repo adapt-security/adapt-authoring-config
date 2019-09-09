39ff2674505023f080542f703ee25bda1474cd27
Thomas Taylor <thomas.taylor@kineo.com>
2019-09-09 22:06:46 +0100

    Add skeleton module test

---
+++ test/module.spec.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const superagent = require('superagent');
+
+describe('Config module', function() {
+  /*
+  superagent
+    .get('/api/config')
+    // .set('X-API-Key', 'foobar')
+    .set('accept', 'json')
+    .end((err, res) => {
+      console.log(err, res);
+      // Calling the end function will send the request
+    });
+    */
+});
