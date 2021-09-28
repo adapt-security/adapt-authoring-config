32e794d7d79e26852ddf3ce70562bd1701e4d2b8
Tom Taylor <hello@tomtaylor.name>
2021-09-28 18:34:00 +0100

    Improve logging in bin script

--- bin/generate-aat-config.js
+++ bin/generate-aat-config.js
@@ -1,23 +1,48 @@
 #!/usr/bin/env node
 /**
  * Generates a template config file which can be populated with required values.
+ * @param {String} [environment] The enviroment to write the config for
  * @param {String} --defaults Will include default values
+ * @param {String} --replace Will override any existing values
+ * @param {String} --update Will update existing configuration with any missing values
  */
  const { Utils } = require('adapt-authoring-core');
  const fs = require('fs-extra');
  const path = require('path');
  const ConfigUtils = require('adapt-authoring-config').Utils;
- 
- const NODE_ENV = process.env.NODE_ENV;
- const useDefaults = Utils.getArgs().defaults;
+ const { 
+   defaults: useDefaults, 
+   params: [env],
+   replace: replaceExisting,
+   update: updateExisting
+ } = Utils.getArgs();
+ const NODE_ENV = env || process.env.NODE_ENV;
  const confDir = path.resolve(path.join(process.cwd(), 'conf'));
  const outpath = path.join(confDir, `${NODE_ENV}.config.js`);
  const configJson = {};
+ const requiredAttrs = [];
  
  async function init() {
+   if(!NODE_ENV) {
+     return console.log(`ERROR: NODE_ENV must be specified\n`);
+   }
+   if(replaceExisting && updateExisting) {
+     return console.log('ERROR: --update and --replace cannot both be specified, please choose one and run the utility again');
+   }
+   if(useDefaults) {
+     return console.log('Default values will be included');
+   }
    try {
      Object.assign(configJson, require(outpath));
-     console.log(`Config already exists for NODE_ENV '${NODE_ENV}'. Any missing values will be added.`);
+     let msg = `Config already exists for NODE_ENV '${NODE_ENV}'. `;
+     if(replaceExisting) {
+       msg += 'All existing values will be replaced.';
+     } if(updateExisting) {
+       msg += 'Any missing values will be added.';
+     } else {
+       return console.log(msg += `Must specified --replace or --update to make changes.`);
+     }
+     console.log(msg);
    } catch(e) {
      console.log(`No config found for NODE_ENV '${NODE_ENV}'. File will be written to ${outpath}\n`);
    }
@@ -26,10 +51,14 @@
      await fs.ensureDir(confDir);
      await fs.writeFile(outpath, `module.exports = ${JSON.stringify(configJson, null, 2)};`);
  
-     console.log('Config file written successfully.\n');
-     console.log('NOTE: any null values will likely need to be specified manually.\n');
+     console.log(`Config file written successfully to ${outpath}.\n`);
+     if(requiredAttrs.length) {
+       console.log('Note: the following required attributes have been given a value of null and must be set for the application to run:');
+       requiredAttrs.forEach(a => console.log(`- ${a}`));
+       console.log('');
+     }
    } catch(e) {
-     console.log(`Failed to write ${outpath}\n${e}`);
+     console.log(`ERROR: Failed to write ${outpath}\n${e}`);
    }
  }
  
@@ -53,6 +82,7 @@
        config.required = schema.required && schema.required.includes(attr);
        if(useDefaults || config.required) {
          memo[attr] = getValueForAttr(attr, config);
+         if(config.required) requiredAttrs.push(`${d}.${attr}`);
        }
        return memo;
      }, {});
@@ -69,6 +99,5 @@
    if(config.required) return null;
    if(config.default) return config.default;
  }
- 
+
  init();
- 
